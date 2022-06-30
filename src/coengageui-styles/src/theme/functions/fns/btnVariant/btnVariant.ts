edaa45372e23601faeee65093e0483bab1c99e87
Lalith Narayan Kashyap <lalith.s@plivo.com>
2022-06-30 17:26:37 +0530

    Button and tooltip added

---
+++ src/coengageui-styles/src/theme/functions/fns/btnVariant/btnVariant.ts
@@ -0,0 +1,65 @@
+import type { CoengageUIColor, CoengageUIGradient, CoengageUIThemeBase } from '../../../types';
+import { rgba } from '../rgba/rgba';
+
+export interface VariantInput {
+  variant:
+    | 'filled'
+    | 'light'
+    | 'outline'
+    | 'default'
+    | 'gradient'
+    | 'white'
+    | 'subtle'
+    | 'info'
+    | 'danger'
+    | 'warning'
+    | 'success';
+  color?: CoengageUIColor;
+  danger?: boolean;
+}
+
+export interface VariantOutput {
+  border?: string;
+  background?: string;
+  color?: string;
+  hover?: string;
+}
+
+export function btnVariant(theme: CoengageUIThemeBase) {
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  return ({ variant, danger }: VariantInput): VariantOutput => {
+    console.log(variant, danger);
+    if (variant === 'subtle') {
+      return {
+        border: danger ? theme.colors.danger[1] : 'transparent',
+        background: danger ? theme.colors.danger[1] : theme.white,
+        color: danger ? theme.white : theme.colors.info[1],
+        hover: danger ? rgba(theme.colors.danger[1], 0.9) : theme.colors.brand[0],
+      };
+    }
+    if (variant === 'outline') {
+      return {
+        border: danger ? theme.colors.danger[1] : theme.colors.info[1],
+        background: theme.white,
+        color: danger ? theme.colors.danger[1] : theme.colors.info[1],
+        hover: danger ? theme.colors.danger[2] : theme.colors.brand[0],
+      };
+    }
+
+    if (variant === 'filled') {
+      return {
+        border: 'transparent',
+        background: danger ? theme.colors.danger[1] : theme.colors.info[1],
+        color: theme.white,
+        hover: danger ? rgba(theme.colors.danger[1], 0.9) : theme.colors.info[2],
+      };
+    }
+
+    return {
+      border: theme.colors.neutral[5],
+      background: theme.white,
+      color: theme.typograhy_dark,
+      hover: theme.colors.neutral[6],
+    };
+  };
+}
